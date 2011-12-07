3c261617e3346383eaa2a7c097fc660319c50cd6
JT Olds <hello@jtolds.com>
2011-12-07 16:23:45 -0700

    better command line options

--- git/treesame_commit.rb
+++ git/treesame_commit.rb
@@ -5,7 +5,7 @@ require 'grit'
 
 class InvalidReferenceError < StandardError; end
 
-def make_treesame(ref, merge=false, repo_path=".", force=false, head="HEAD", message=nil)
+def make_treesame(ref, merge=false, repo_path=".", force=false, message=nil)
   git_dir = `cd #{repo_path} && git rev-parse --git-dir`.strip
   raise Grit::InvalidGitRepositoryError.new(repo_path) if git_dir.empty?
   git_dir = File.expand_path(git_dir)
@@ -14,7 +14,7 @@ def make_treesame(ref, merge=false, repo_path=".", force=false, head="HEAD", mes
   grit_repo = Grit::Repo.new git_dir, :is_bare => true
 
   commit = grit_repo.commit(ref) || (raise InvalidReferenceError.new(ref))
-  parent_commit = grit_repo.commit(head)
+  parent_commit = grit_repo.commit("HEAD")
   return if commit.tree.id == parent_commit.tree.id and !force
   parents = [parent_commit.id]
   parents << commit.id if merge
@@ -34,28 +34,33 @@ def make_treesame(ref, merge=false, repo_path=".", force=false, head="HEAD", mes
   new_commit = Grit::GitRuby::Commit.new(commit.tree.id, parents, author, committer, message, {}, repo)
   new_commit_sha1 = repo.put_raw_object(new_commit.raw_content, new_commit.type.to_s)
 
-  if head == "HEAD"
-    path = File.join(git_dir, `cd #{repo_path} && git symbolic-ref HEAD`.strip)
-    FileUtils.mkdir_p(File.dirname(path))
-    File.open(path, 'w') { |f| f.write(new_commit_sha1) }
-
-    `cd #{repo_path} && git reset --hard HEAD`
-  end
-
+  `cd #{repo_path} && git reset --hard #{new_commit_sha1}`
   return new_commit_sha1
 end
 
 if __FILE__ == $0
-  ref = ARGV.shift
-  if ref == "--help"
-    puts "usage: git-treesame-commit [--merge] <ref>
+  args = []
+  merge = false
+  force = false
+  usage = "usage: git-treesame-commit [--merge, --force] <ref>
 
 given a ref, git-treesame-commit <ref> will create a new merge commit that has
 the exact same treehash as the given ref on your current branch. if --merge is
 provided, <ref> is added as a parent commit."
-  elsif ref == "--merge"
-    make_treesame ARGV[0], true
-  else
-    make_treesame ref
+
+  ARGV.each do |arg|
+    if arg == "--help"
+      abort usage
+    elsif arg == "--merge"
+      merge = true
+    elsif arg == "--force"
+      force = true
+    else
+      args << arg
+    end
   end
+
+  abort usage if args.size != 1
+
+  make_treesame args[0], merge, ".", force
 end
